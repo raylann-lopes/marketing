47ec9e0320f38b42fc28bb84e28c9e89f942abbf
raylann-lopes <raylannlopes@gmail.com>
2026-03-25 20:28:58 -0300

    feat: adicionar entidade User e camada de repositório

---
+++ src/main/java/com/north/producoes/entity/UserEntity.java
@@ -0,0 +1,38 @@
+package com.north.producoes.entity;
+
+import com.north.producoes.entity.postStatusEnum.RoleEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleEnum role = RoleEnum.USER;
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime created_at;
+}
---
+++ src/main/java/com/north/producoes/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.north.producoes.repository;
+
+import com.north.producoes.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findById(Long id);
+    Optional<UserEntity> findByEmail(String email);
+    List<UserEntity> findByName(String name);
+}
