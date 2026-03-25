905cbbe81764be8dba7f504567fb000eb952bf1e
raylann-lopes <raylannlopes@gmail.com>
2026-03-25 20:31:46 -0300

    feat: inicializar projeto com estrutura base Spring Boot e configurações

---
+++ src/main/java/com/north/producoes/ProducesApplication.java
@@ -0,0 +1,13 @@
+package com.north.producoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/north/producoes/ProducesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.north.producoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
