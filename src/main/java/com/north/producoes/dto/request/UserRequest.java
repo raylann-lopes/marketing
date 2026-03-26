cb778e247c02837198a613f900bc2e59ac6d04a7
raylann-lopes <raylannlopes@gmail.com>
2026-03-25 23:22:49 -0300

    feat: adicionar DTOs de usuário e corrigir UserRepository

---
+++ src/main/java/com/north/producoes/dto/request/UserRequest.java
@@ -0,0 +1,10 @@
+package com.north.producoes.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRequest(
+   @NotBlank String name,
+   @NotBlank @Email String email,
+   @NotBlank String password
+) {}
---
+++ src/main/java/com/north/producoes/dto/response/UserResponse.java
@@ -0,0 +1,17 @@
+package com.north.producoes.dto.response;
+
+import com.north.producoes.entity.UserEntity;
+
+public record UserResponse(
+        Long id,
+        String name,
+        String email
+) {
+    public static UserResponse from(UserEntity entity) {
+        return new UserResponse(
+                entity.getId(),
+                entity.getName(),
+                entity.getEmail()
+        );
+    }
+}
