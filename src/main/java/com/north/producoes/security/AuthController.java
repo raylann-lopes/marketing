9aa0927b56fbaf5e4802d750780a1af38824aac6
raylann-lopes <raylannlopes@gmail.com>
2026-03-26 18:53:14 -0300

    feat: adiciona rota de registro e configura autenticação JWT no   Swagger

--- src/main/java/com/north/producoes/security/AuthController.java
+++ src/main/java/com/north/producoes/security/AuthController.java
@@ -1,14 +1,17 @@
 package com.north.producoes.security;
 
 import com.north.producoes.dto.request.LoginRequest;
+import com.north.producoes.dto.request.UserRequest;
 import com.north.producoes.dto.response.LoginResponse;
+import com.north.producoes.entity.UserEntity;
+import com.north.producoes.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,21 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
-    private final UserDetailsService userDetailsService;
+    private final UserService userService;
     private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody @Valid
-LoginRequest loginRequest){
-
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.email(), loginRequest.password()));
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.email());
+        UserDetails userDetails = userService.loadUserByUsername(loginRequest.email());
         String token = jwtService.generateToken(userDetails);
 
         return ResponseEntity.ok(new LoginResponse(token));
-
     }
 }
--- src/main/java/com/north/producoes/security/JwtFilter.java
+++ src/main/java/com/north/producoes/security/JwtFilter.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.lang.NonNull;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
