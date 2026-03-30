bdd30965223139fc8eddd5351d059f7f5a9a5bb5
raylann-lopes <raylannlopes@gmail.com>
2026-03-29 22:28:30 -0300

    fix: corrigir importação de classe User

--- src/main/java/com/north/producoes/repository/UserRepository.java
+++ src/main/java/com/north/producoes/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.north.producoes.repository;
 
 import com.north.producoes.entity.UserEntity;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
